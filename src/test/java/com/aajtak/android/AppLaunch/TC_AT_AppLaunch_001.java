72b5a1fe4912243093966c16f4b9a481343ffd03
DwarakRaj <sathya_dwarak@yahoo.co.in>
2023-05-16 15:27:58 +0530

    added dir

---
+++ src/test/java/com/aajtak/android/AppLaunch/TC_AT_AppLaunch_001.java
@@ -0,0 +1,28 @@
+
+package com.aajtak.android.AppLaunch;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.aajtak.android.init.Aajtak_app_Util;
+
+/**
+ * Date : 1 June 2021 This is App Launch module of AajTak Application 
+ * Description: User should be able to view the first onboarding screen with Shuru Karien button when user launch the App.
+ * @author Archana George
+ * @version 9.37(417)
+ */
+
+@Listeners(com.aajtak.android.listener.MyExtentListners.class)
+public class TC_AT_AppLaunch_001 extends Aajtak_app_Util {
+
+	@Test(description = "Description: Verify that user is able to view the first onboarding screen with Shuru Karien button when user launch the App.", priority = 1)
+	public void AT_AppLaunch_001() throws Exception {
+		// launch the App
+		launchApp();
+		
+		// Validation if user is able to view shuru karien button
+		isElementExist(shuruKarienOnboarding.getShuruKarein()); 
+	}
+
+}
